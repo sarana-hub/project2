d385b3f141183c94cb88ea5510347398050770f3
nayeon-jo <nyc0516@naver.com>
2023-06-18 21:52:18 +0900

    완성3

--- src/main/java/hello/springmvcjpa/domain/login/SessionConst.java
+++ src/main/java/hello/springmvcjpa/domain/login/SessionConst.java
@@ -2,6 +2,4 @@ package hello.springmvcjpa.domain.login;
 
 public interface SessionConst {
     public static String LOGIN_CUSTOMER="loginCustomer";
-
-    //public static String LOGIN_SHOP="loginShop";
 }
